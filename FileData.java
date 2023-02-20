22ed0f50b8b3a255e148955b69b5ef1b4d8cd781
morozvitali <morozvitali99@gmail.com>
2023-02-21 00:11:29 +0200

    init#2

--- FileData.java
+++ FileData.java
@@ -4,4 +4,16 @@ public class FileData {
     // Клас представляє конкретний файл і складається з:
     // ім'я файлу, розміру в байтах, шлях до файлу.
 
+    private String name;
+    private byte size;
+    private String path;
+
+    //конструктор
+
+        public FileData () {
+            this.name = name;
+            this.size = size;
+            this.path = path;
+        }
+
 }
